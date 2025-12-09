f71c3ff53111947c07b639428805282df70171fe
Saad Rimeh <saad.rimeh.2001.01@gmail.com>
2025-12-09 14:30:24 +0300

    String slicing ✂️

---
+++ String slicing ✂️/index.js
@@ -0,0 +1,26 @@
+//const fullName ="Saad Rimeh";
+
+//let firstName = fullName.slice(0 , 4);
+//let lastName = fullName.slice(5 , 9);
+
+//console.log(fullName.length)
+
+//let firstChar=fullName.slice(0,1);
+//let lastChar= fullName.slice(-1);
+
+
+//console.log(firstName);
+//console.log(lastName);
+
+//console.log("***********");
+
+//console.log(firstChar);
+//console.log(lastChar);
+////////////////////////////////////////////////
+
+const email = "Saad@gmail.com";
+
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@"));
+console.log(username);
+console.log(extension);
